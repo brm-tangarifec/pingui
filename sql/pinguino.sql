c0a02d923f6184cfdf6a9d1a7e68dc5a13881d1b
cristian <cristian.tangarife@brm.com.co>
2016-09-26 10:27:09 -0500

    cambios en el sql

--- sql/pinguino.sql
+++ sql/pinguino.sql
@@ -1,16 +1,16 @@
 /*
 Navicat MySQL Data Transfer
 
-Source Server         : migracion
-Source Server Version : 50505
-Source Host           : localhost:3306
-Source Database       : pinguino
+Source Server         : fbapp
+Source Server Version : 50169
+Source Host           : 50.57.130.144:3306
+Source Database       : per16_chivasdb
 
 Target Server Type    : MYSQL
-Target Server Version : 50505
+Target Server Version : 50169
 File Encoding         : 65001
 
-Date: 2016-09-14 15:55:16
+Date: 2016-09-26 10:19:30
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -157,11 +157,12 @@ CREATE TABLE `tb_session` (
   `dns` varchar(128) NOT NULL,
   `set_time` varchar(10) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=48 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_session
 -- ----------------------------
+INSERT INTO `tb_session` VALUES ('47', '1', 'dO0WZmZT1/8t/Td/V76mXYMve66AWHYTvTqFB+1/9J4=', 'a42a463470315ff5a1b5307a081b9031a6ce28fba98adb506c7a600c72ed93b7a250870967f677f8d74861032f303f13a72cbb40a01df4f9e836422a71878b1b', 'fbapp.brm.com.co', '1474659990');
 
 -- ----------------------------
 -- Table structure for tb_usuario
@@ -175,16 +176,17 @@ CREATE TABLE `tb_usuario` (
   `documento` varchar(20) NOT NULL,
   `telefono` varchar(25) NOT NULL,
   `email` varchar(150) NOT NULL,
-  `idProvincia` int(11) NOT NULL,
-  `idCiudad` int(11) NOT NULL,
+  `provincia` varchar(100) NOT NULL,
+  `ciudad` varchar(100) NOT NULL,
   `activo` enum('S','N') NOT NULL,
   `contrasena` varchar(100) NOT NULL,
   `idFacebook` varchar(50) DEFAULT NULL,
   `fecha` datetime NOT NULL,
   `fechaActualizacion` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Records of tb_usuario
 -- ----------------------------
+INSERT INTO `tb_usuario` VALUES ('1', 'Cristian', 'Tangarife', '0000-00-00', '', '', 'cgt9145@gmail.com', 'yatusabe', 'Ciudad', 'S', 'QW51YmlzMTQ1OSQ=', '10153969802438226', '2016-09-22 15:30:25', null);
