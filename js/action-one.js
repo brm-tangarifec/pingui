6e0771c527ee3aa866078129be245224c24d485e
Mateo Quintero <mateo.quintero@brm.com.co>
2016-09-27 09:14:50 -0500

    ajustes diseño

--- js/action-one.js
+++ js/action-one.js
@@ -1,13 +1,19 @@
 var action="";
 var device="desktop";
 switch(device) {
+   case "phone-desktop":
+  		$("#box").remove();
+  		$("article").addClass(device);
+			action="gira tu celular 360 grados";
+			Sequencer.init({from:1, to: 162, folder:"img/action-one", baseName:"action-one-", ext:"jpg"});
+	break;
 	case "mobile":
 			action="gira tu celular 360 grados";
 			Sequencer.init({from:1, to: 162, folder:"img/action-one", baseName:"action-one-", ext:"jpg"});
-	 break;
+	break;
 	case "desktop":
 			action="Mover mouse arriba o abajo";
-			Sequencer.init({from:1, to: 162, folder:"img/action-one", baseName:"action-one-", ext:"jpg"});
+			Sequencer.init({from:1, to: 162, folder:"img/action-one", baseName:"action-one-", ext:"jpg",direction:"y",playMode:"mouse"});
 	break;
 }
 
