e4724eaf47fd2e847dad338e19d906c404580ef1
brm-barreraj <barreraj@iMac-de-Brm.local>
2016-09-28 00:43:25 -0500

    Cambio de mensajes en el los clientes del socket

--- js/clientCel.js
+++ js/clientCel.js
@@ -12,7 +12,7 @@ function comparaCodigo(){
         }
         socket.emit('comparaCodigo', data);
     }else{
-        alert("Ingrése código");
+        $("#message").html("Ingrése el código");
     }
 }
 
@@ -39,55 +39,23 @@ if (screen.width<1280)
         if (data.estadoCodigo==1) {
             // Sincronizo correctamente
             estadoSincro=1;
-            alert("Se sincronizó correctamente");
+            $("#message").html("Se sincronizó correctamente");
             createcanvas(device);
         }else if (data.estadoCodigo==2){
             // El código ya está en uso
-            alert("El código ya se está usando");
+            $("#message").html("El código ya se está usando");
             estadoSincro=0;
         }else{
             // El código es incorrecto
-            alert("El código es incorrecto");
+            $("#message").html("El código es incorrecto");
             estadoSincro=0;
         }
     });
     
     //Paso 11 - Se desconecta Web
     socket.on('desconectadoWeb', function () {
-        alert("Se desconecto el cliente web");
+        $("#message").html("Se desconecto el cliente web");
         estadoSincro=0;
-        $("#conectado").hide();
-        $("#desconectado").show();
+        location.reload();
     });
-}
-
-
-// Accion
-if (screen.width < 1280) 
-{
-    window.addEventListener('load', function(){
-        var box1 = document.getElementById('box1')
-        var startx = 0
-        var dist = 0
-        box1.addEventListener('touchstart', function(e){
-            var touchobj = e.changedTouches[0] // reference first touch point (ie: first finger)
-            startx = parseInt(touchobj.clientX) // get x position of touch point relative to left edge of browser
-            realizaAccion(startx)
-            e.preventDefault()
-        }, false)
-     
-        box1.addEventListener('touchmove', function(e){
-            var touchobj = e.changedTouches[0] // reference first touch point for this event
-            var dist = parseInt(touchobj.clientX) - startx
-            realizaAccion(dist)
-            e.preventDefault()
-        }, false)
-     
-        box1.addEventListener('touchend', function(e){
-            var touchobj = e.changedTouches[0] // reference first touch point for this event
-            realizaAccion(touchobj.clientX)
-            e.preventDefault()
-        }, false)
-     
-    }, false)
 }
