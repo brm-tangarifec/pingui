f848774a9f673e342c89104c5dcaef96a39d691f
cristian <cristian.tangarife@brm.com.co>
2016-09-28 03:03:32 -0500

    Cambios para el cambio de pass

--- js/rp-alert.js
+++ js/rp-alert.js
@@ -13,3 +13,32 @@ $(function(){
 		$("#cream-jar").addClass("animated zoomIn");
 	}, 2000);
 });
+
+jQuery(document).ready(function(){
+  /*Cambio de contraseña*/
+  jQuery("#changeP").click(function(){
+    console.log("Hola");
+    var pass=jQuery("#lepas").val(),
+    passc=jQuery("#lepasc").val();
+    urlR='eventos.php';
+
+    jQuery.ajax({
+            url: urlR,
+            dataType:'json' ,
+            type: 'POST',
+            data:{
+              email:email,
+              vrtCrt:'changeP'
+            },
+            success: function (data){
+              console.log(data);
+              window.location="perfil.php";
+            }, 
+            error: function(result) {
+                      console.log(result,'error');
+              }
+
+            });
+            return false;
+
+});
