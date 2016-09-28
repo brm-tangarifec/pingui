a6ef70104fbcb862ea447e26b07f6f176f35e10f
Mateo Quintero <mateo.quintero@brm.com.co>
2016-09-27 19:21:21 -0500

    cambio sincronizar

--- js/synchronize.js
+++ js/synchronize.js
@@ -63,12 +63,8 @@ function createcanvas(device){
 
 	    case "phone-desktop":
 					
-					if (mobile) { 
-						$("article").addClass(device); 
-					} else { 
-						$("#box-action").show(); 
-						$("#action").show();
-					}
+					if (mobile) { $("article").addClass(device); }
+					if (!mobile) { $("#box-action").show(); $("#action").show(); }
 	    		action="Desliza tu dedo izquierda o derecha";
 					Sequencer.init({from:0, to: 123, folder:"img/action-two", baseName:"action-two-", ext:"jpg"});
 
@@ -84,7 +80,7 @@ function createcanvas(device){
 	    break;
 
 	    case "desktop":
-	    
+
 					$("#box-action").show(); 
 					$("#action").show();
 	    		action="Mover mouse izquierda o derecha";
